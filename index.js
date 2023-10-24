a9910366a8a0f4176638b811d457e2b6ce804bdd
unknown <mikimaku69@gmail.com>
2023-10-25 00:03:51 +0600

    invitation link generated

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser =require('body-parser');
+const nodemailer =require('nodemailer');
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+
+
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 
 const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.w68aaz5.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -42,6 +47,9 @@ async function run(){
           res.send(student);
          })
 
+        //  get invitation email 
+        
+
 
         // stored the new classes 
          app.post('/user', async(req,res) =>{
@@ -54,22 +62,54 @@ async function run(){
 
          //stored people in new classes database
          app.post('/student', async(req,res)=>{
-          const newStudent=req.body;
-          console.log("new students added",newStudent);
-          const result1=await newStudentCollection.insertOne(newStudent);
-          res.send(result1);
+          const { recipent}=req.body;
+
+
+          // owner email and password 
+          const emailUser=process.env.emailUser;
+          const emailPassword=process.env.emailPassword;
+
+
+          const transporter =nodemailer.createTransport({
+            service: 'gmail',
+            auth:{
+              user: emailUser,
+              pass: emailPassword
+            }
+          });
+          const mailOption ={
+            from: emailUser,
+            to: recipent,
+            subject: "Class invitation link",
+            text: 'This is a test email'
+          };
+          transporter.sendMail(mailOption,(error,info)=>{
+            if(error){
+              console.log(error);
+              res.status(500).json({ success: false, message: 'Failed to send invitation' });
+            }
+            else {
+              console.log('Email sent: ' + info.response);
+              res.json({ success: true, message: 'Invitation sent successfully' });
+            }
+          })
+
+          //storing in database
+          const result1 = await newStudentCollection.insertOne({ email: recipent });
+          res.json({ success: true, message: 'Invitation sent successfully', result: result1 });
+
 
          })
 
            
 
-    }
+        }
     finally{
       
     }
    
     
-}
+  }
 run().catch(console.dir);
 
 
